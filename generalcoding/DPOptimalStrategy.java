abb447c63fc4d515da7d6b79c46b85486069a9a7
KunjalBhatt <bhattkunjal13@yahoo.com>
2023-04-02 21:11:44 +0530

    first Files

---
+++ generalcoding/DPOptimalStrategy.java
@@ -0,0 +1,45 @@
+public class DPOptimalStrategy{
+    public static void main(String[] args) {
+        int arr[] = {8,15,3,7};
+
+        System.out.println("This ans = "+countMaximum(arr, arr.length));
+        System.out.println("This Dp ans = "+countMaximumDP(arr, arr.length));
+    }
+    static long countMaximum(int arr[], int n){
+
+        return countMaximumRe(arr, 0, n-1);
+    }
+
+    static long countMaximumRe(int arr[],int i, int j){
+        if(i+1 == j) return Math.max(arr[i], arr[j]);
+        
+        return Math.max(arr[i] + Math.min(countMaximumRe(arr, i+2, j), countMaximumRe(arr, i+1, j-1)),
+                arr[j] + Math.min(countMaximumRe(arr, i, j-2), countMaximumRe(arr, i+1, j-1))
+            );
+    };
+
+
+    static long countMaximumDP(int arr[], int n){
+        long[][] dp = new long[n][n];
+
+        for(int gap = 1;gap < n; gap+=2){
+            for(int i = 0; i+gap < n; i++){
+                int j = i +gap;
+                if(gap == 1){
+                    dp[i][j] = Math.max(arr[i], arr[j]);
+                }else{ 
+                    dp[i][j] = Math.max(arr[i] + Math.min(dp[i+2][j],dp[i+1][j-1]),
+                         arr[j] + Math.min(dp[i][j-2],dp[i+1][j-1])
+                    );
+                }
+            }
+        }
+
+        /* for(long a[] : dp){
+            System.out.println(Arrays.toString(a));
+        } */
+
+        return dp[0][n-1];
+
+    }
+}
---
+++ generalcoding/DPnCr.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class DPnCr{
+
+    public static void main(String[] args) {
+        int n = 5;
+        int r = 4;
+        System.out.println("Ans = "+findCombincation(n, r));
+    }
+    static int findCombincation(int n, int r){
+        if(r > n) return 0;
+
+        long dp[] = new long[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i]= dp[i-1] * i;
+        }
+        long c = dp[n] /(dp[r] * dp[n - r]);
+        System.out.println(c);
+        return (int) c % (1000000000+7);
+
+    }
+}
